96a05efe8ae195c65ac4cb21e4c3ea19f89a79d4
alex-nestor <nestorisena2001@gmail.com>
2020-12-12 17:15:16 +0200

    Add datetime and descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_110949) do
+ActiveRecord::Schema.define(version: 2020_12_12_130351) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_12_12_110949) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "starts_at"
+    t.text "description"
   end
 
 end
