429ccf0c8642b9ff4552ea198ffc14b79484af08
alex-nestor <nestorisena2001@gmail.com>
2020-12-13 20:59:02 +0200

    Add new events page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'events#index'
-
-  get 'events' => 'events#index'
-  get 'events/:id' => 'events#show', as: 'event'
-  get 'events/:id/edit' => 'events#edit', as: 'edit_event'
-  patch 'events/:id' => 'events#update'
+  
+  resources :events
+  
+  # get 'events' => 'events#index'
+  # get 'evens/new' => 'events#new'
+  # get 'events/:id' => 'events#show', as: 'event'
+  # get 'events/:id/edit' => 'events#edit', as: 'edit_event'
+  # patch 'events/:id' => 'events#update'
 end
