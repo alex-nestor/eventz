8df556bd635a1770f739decf11cf08ef2f51157b
alex-nestor <nestorisena2001@gmail.com>
2020-12-14 17:19:32 +0200

    Validations and errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,8 +13,11 @@ class EventsController < ApplicationController
 
   def update
     @event = Event.find(params[:id])
-    @event.update(event_params)
-    redirect_to @event
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
   end
 
   def new
@@ -23,8 +26,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.save
-    redirect_to @event
+    if @event.save
+      redirect_to @event
+    else 
+      render :new
+    end
   end
 
   def destroy
@@ -37,6 +43,6 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event)
-          .permit(:name, :description, :location, :price, :starts_at)
+          .permit(:name, :description, :location, :price, :starts_at, :capacity, :image_file_name)
   end
 end
