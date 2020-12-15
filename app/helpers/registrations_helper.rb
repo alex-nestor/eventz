5127801ed9996a58da19b08213c8bb4402893052
alex-nestor <nestorisena2001@gmail.com>
2020-12-16 01:29:20 +0200

    Add sold-out feature

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,10 @@
 module RegistrationsHelper
+  def sold_out_or_register?(event)
+    if event.sold_out?
+      content_tag(:span, 'Sold Out!', class: 'sold-out')
+    else
+      link_to 'Register!', new_event_registration_path(event),
+              class: 'register'
+    end
+  end
 end
