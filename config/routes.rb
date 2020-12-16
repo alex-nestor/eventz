ae9ca150c976c392d775590d1b35a15cfc1900ae
alex-nestor <nestorisena2001@gmail.com>
2020-12-16 21:31:16 +0200

    Add user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     resources :registrations
   end
 
+  resources :users
+  get 'signup' => 'users#new'
+
   # get 'events' => 'events#index'
   # get 'evens/new' => 'events#new'
   # get 'events/:id' => 'events#show', as: 'event'
